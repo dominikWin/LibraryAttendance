ce4cc97bd85bc97913a7f241c8268556e027bb94
Dominik Winecki <dominikwinecki@gmail.com>
2016-03-13 15:51:05 -0400

    Added visit logging

--- includes/database.php
+++ includes/database.php
@@ -1,12 +1,61 @@
 <?php
 include("config/dbconfig.php");
 
+$db1conn = null;
+$db2conn = null;
+
+function db1_connect() {
+	global $db1;
+	global $db1conn;
+	$servername = $db1['name'];
+	$username = $db1['uname'];
+	$password = $db1['passwd'];
+	$dbname = $db1['dbname'];
+	try {
+		$db1conn = new PDO("mysql:host=".$db1['name'].";dbname=".$db1['dbname'], $db1['uname'], $db1['passwd']);
+		$db1conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e) {
+		die($e);
+	}
+}
+
+function db2_connect() {
+	global $db2;
+	global $db2conn;
+	$servername = $db2['name'];
+	$username = $db2['uname'];
+	$password = $db2['passwd'];
+	$dbname = $db2['dbname'];
+	try {
+		$db2conn = new PDO("mysql:host=".$db2['name'].";dbname=".$db2['dbname'], $db2['uname'], $db2['passwd']);
+		$db2conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e) {
+		die($e);
+	}
+}
+
+function db1_close() {
+
+}
+
+function db2_close() {
+
+}
+
 function isValidID($id) {
 
 }
 
 function logVisit($id) {
-	
+	global $db2conn;
+	$stmt = $db2conn->prepare("INSERT INTO visits (student_id, timestamp)
+        VALUES (:id, :time)");
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':time', $time);
+	$time = time();
+	$stmt->execute();
 }
 
 function getPastUsers($number=25) {
--- insert.php
+++ insert.php
@@ -1,5 +1,8 @@
 <?php
 
-print_r($_POST);
+// print_r($_POST);
+include("includes/database.php");
+// print_r($db2);
 
- ?>
+db2_connect();
+logVisit(htmlspecialchars($_POST['q']));
