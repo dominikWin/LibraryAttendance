62256ecbb9a414f9c49d44c84b8dd831d9a984b2
Dominik Winecki <dominikwinecki@gmail.com>
2016-03-14 16:01:36 -0400

    Seperated login and numberfield

--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-
-	$("#q").keypress(validateNumber);
+	// $("#q").keypress(validateNumber);
 
 	$("#login").submit(function(event) {
 		//Form submited
@@ -25,18 +24,3 @@ $(document).ready(function(){
 	})
 
 })
-
-//Thanks stackoverflow
-function validateNumber(event) {
-	//If this fails see http://stackoverflow.com/questions/469357/html-text-input-allow-only-numeric-input
-	var key = window.event ? event.keyCode : event.which
-	if (event.keyCode === 8 || event.keyCode === 46
- 	|| event.keyCode === 37 || event.keyCode === 39
-	|| event.keyCode === 13) {
-		return true
-	}
-	else if ( key < 48 || key > 57 ) {
-		return false
-	}
-	else return true
-};
---
+++ js/numberfield.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$("[numonly]").keypress(validateNumber)
+})
+
+//Thanks stackoverflow
+function validateNumber(event) {
+	//If this fails see http://stackoverflow.com/questions/469357/html-text-input-allow-only-numeric-input
+	var key = window.event ? event.keyCode : event.which
+	if (event.keyCode === 8 || event.keyCode === 46
+ 	|| event.keyCode === 37 || event.keyCode === 39
+	|| event.keyCode === 13) {
+		return true
+	}
+	else if ( key < 48 || key > 57 ) {
+		return false
+	}
+	else return true
+};
