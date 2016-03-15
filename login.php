ed4e48d5c7cc68a75119a9f7b06b0a67509836a0
Dominik Winecki <dominikwinecki@gmail.com>
2016-03-15 19:51:59 -0400

    Add Bootstrap and rebuild login page

--- login.php
+++ login.php
@@ -2,20 +2,40 @@
 $config = include("includes/config.php");
 ?>
 
+<!DOCTYPE html>
 <html>
-    <head>
-        <title><?php echo $config->name; ?></title>
-        <script type="text/javascript" src="js/jquery-2.2.1.min.js"></script>
-        <script type="text/javascript" src="js/login.js"></script>
-        <script type="text/javascript" src="js/numberfield.js"></script>
-    </head>
-    <body>
-            <h1><?php echo $config->name; ?></h1>
-            <form id="login" action="insert.php" method="post">
-                    Login:
-                    <input id="q" name="q" type="text" step="1" maxlength="6" size=4 numonly>
-            </form>
-            <div id="result">
-            </div>
-    </body>
+	<head>
+		<title><?php echo $config->name; ?></title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<script type="text/javascript" src="js/jquery-2.2.1.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	        <script type="text/javascript" src="js/login.js"></script>
+	        <script type="text/javascript" src="js/numberfield.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="login.php"><?php echo $config->name; ?></a>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a>Item1</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="container">
+			<!-- <div class="col-md-6"></div> -->
+			<!-- Login Menu -->
+			<div class="col-lg-6">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Login
+					</div>
+					<div class="panel-body">
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
