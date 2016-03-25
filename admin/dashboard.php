19b853d71c0fb98f5dfe2fe3fa6dbb982cb7c5d8
Dominik Winecki <dominikwinecki@gmail.com>
2016-03-25 15:30:12 -0400

    Created include for admin verify

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,38 +1,5 @@
 <?php
-
-if(!isset($_COOKIE['sid'])) {
-	include($_SERVER['DOCUMENT_ROOT']."/includes/fail.php");
-}
-
-$hash = $_COOKIE['sid'];
-
-if(strlen($hash) < 40 || strlen($hash) > 40) {
-	echo "Hash is invalid length";
-	exit();
-}
-
-if(!ctype_alnum($hash)) {
-	echo "sid is not valid";
-	exit();
-}
-
-include($_SERVER['DOCUMENT_ROOT']."/includes/database.php");
-
-$status = db2_connect();
-
-if(is_null($status)) {
-	echo json_encode(array("status" => 30, "error" => "Error connecting to database"));
-	exit();
-}
-
-$name = verifySession($_COOKIE['sid']);
-
-if($name === null) {
-	include($_SERVER['DOCUMENT_ROOT']."/includes/fail.php");
-}
-
-$config = include($_SERVER['DOCUMENT_ROOT']."/includes/config.php");
-//If execution gets here display dashboard
+include($_SERVER['DOCUMENT_ROOT']."/includes/verifyAdmin.php");
 ?>
 
 <!DOCTYPE html>
