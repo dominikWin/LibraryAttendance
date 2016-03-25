986b896e88ca68522ce8e0860e4172e0a2bbfdaa
Dominik Winecki <dominikwinecki@gmail.com>
2016-03-25 14:00:29 -0400

    Extract head contents to seperate file

--- login.php
+++ login.php
@@ -5,15 +5,7 @@ $config = include("includes/config.php");
 <html>
 	<head>
 		<title><?php echo $config->name; ?></title>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-		<script type="text/javascript" src="js/jquery-2.2.1.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	        <script type="text/javascript" src="js/login.js"></script>
-		<script type="text/javascript" src="js/adminLogin.js"></script>
-	        <script type="text/javascript" src="js/numberfield.js"></script>
-		<script type="text/javascript" src="js/js.cookie-2.1.0.min.js"></script>
-		<script type="text/javascript" src="js/cookie.js"></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php include($_SERVER['DOCUMENT_ROOT']."/includes/head.php"); ?>	
 	</head>
 	<body>
 		<nav class="navbar navbar-default">
