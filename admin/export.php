56b01d3bbc3d40f4c18ee259cf408be77e889d46
Dominik Winecki <dominikwinecki@gmail.com>
2016-04-27 16:45:53 -0400

    Add export setup

--- admin/export.php
+++ admin/export.php
@@ -1,5 +1,22 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT']."/includes/verifyAdmin.php");
+
+if(isset($_GET['format'])) {
+	//Gen system
+	if($_GET['format'] == 'csv') {
+
+	}
+	elseif($_GET['format'] == 'xml') {
+
+	}
+	elseif($_GET['format'] == 'json') {
+
+	}
+	else {
+		die("Unknown format");
+	}
+	exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +27,11 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/verifyAdmin.php");
 		<?php include($_SERVER['DOCUMENT_ROOT']."/includes/head.php"); ?>
 		<link href="/css/sb-admin/sb-admin.css" rel="stylesheet">
 		<script type="text/javascript" src="/js/numberfield.js"></script>
+		<script>
+			$(document).ready(function(){
+    				$('[data-toggle="tooltip"]').tooltip();
+			});
+		</script>
 	</head>
 	<body>
 		<div id="wrapper">
@@ -18,40 +40,34 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/verifyAdmin.php");
 				<h1>Export</h1>
 				<hr>
 				<div class="container">
-					<div class="col-md-6">
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								Export Settings
-							</div>
-							<div class="panel-body">
-								<form class="form" method="get">
-									<div class="form-group" id="format" name="format">
-										<label>Format:</label>
-										<select class="form-control">
-											<option value="one">CSV</option>
-											<option value="two">XML</option>
-											<option value="three">JSON</option>
-										</select>
-									</div>
-									<div class="form-group">
-										<label>Limit Quantity:</label>
-										<div class="checkbot"><label><input type="checkbox" id="limit"></label></div>
-										<input class="form-control" autocomplete="off" name="num" id="num" type="number" min="0" max="1000" step="1" placeholder="Quantity"></input>
-									</div>
-									<div class="form-group">
-										<label>Fields:</label>
-										<select multiple class="form-control">
-											<option>First Name</option>
-											<option>Last Name</option>
-											<option>ID</option>
-											<option>Time (Formated)</option>
-											<option>Time (UNIX)</option>
-										</select>
-									</div>
-									<div class="form-group">
-										<button type="submit" class="btn btn-primary form-control">Export</button>
-									</div>
-								</form>
+					<div class="row">
+						<div class="span2 offset3">
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									Export Settings
+								</div>
+								<div class="panel-body">
+									<form class="form" method="get">
+										<div class="form-group">
+											<label>Format:</label>
+											<select class="form-control" name="format">
+												<option value="csv">CSV</option>
+												<option value="xml">XML</option>
+												<option value="json">JSON</option>
+											</select>
+										</div>
+										<div class="form-group">
+											<div class="checkbox">
+												<label>
+													<input type="checkbox" name="unix" />
+													<a href="#" data-toggle="tooltip" style="text-decoration:none;" data-placement="auto" title="Will display time as the seconds since Jan 1, 1970. If unsure don't select.">UNIX Time Format</a>
+												</label>
+										</div>
+										<div class="form-group">
+											<button type="submit" class="btn btn-primary form-control">Export</button>
+										</div>
+									</form>
+								</div>
 							</div>
 						</div>
 					</div>
