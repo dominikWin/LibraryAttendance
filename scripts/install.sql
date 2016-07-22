63e383d5466c3b63b373f88e28a1d97a1edfbff4
Dominik Winecki <dominikwinecki@gmail.com>
2016-07-22 00:58:32 +0000

    Add SQL scripts

---
+++ scripts/install.sql
@@ -0,0 +1,13 @@
+create database students;
+
+create table students.students (id INT, fname VARCHAR(32), lname VARCHAR(32));
+
+create database library;
+
+create table library.visits (id INT AUTO_INCREMENT UNIQUE KEY, student_id INT PRIMARY KEY, timestamp INT UNSIGNED);
+
+create table library.admins (id INT AUTO_INCREMENT PRIMARY KEY, uname VARCHAR(32), passwd CHAR(120));
+
+create table library.admin_sessions(user_id INT AUTO_INCREMENT PRIMARY KEY, hash CHAR(40), timestamp INT UNSIGNED, expire INT);
+
+insert into `library`.`admins`(`id`, `uname`, `passwd`) values ('1','root','$2a$08$75usl/NNzioG9BxUdxiMwOkhESjx0iPhkZpfBzQww8wyRMPibpf0m');
---
+++ scripts/sample_students.sql
@@ -0,0 +1,3 @@
+insert into students.students (id,fname,lname) values (1,'fname1','fname1');
+insert into students.students (id,fname,lname) values (2,'fname2','fname2');
+insert into students.students (id,fname,lname) values (3,'fname3','fname3');
